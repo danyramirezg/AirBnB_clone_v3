980c141fedade8bb1397c711be2230df2b8349cb
Daniel C <51679787+dacastanogo@users.noreply.github.com>
2020-01-30 16:46:33 -0500

    Revert "Added City views"

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for import things"""
+"""Module"""
 
 from flask import Blueprint
 
@@ -7,4 +7,3 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.status import *
